5690dd1d67ff19dafebf8744c6df4f513c9c9d91
sentimental-j <83724982+sentimental-j@users.noreply.github.com>
2025-05-02 19:10:56 -0500

    Update ParticlesBackground.tsx

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -1,1 +1,54 @@
+// components/ParticlesBackground.tsx
+"use client";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
+export default function ParticlesBackground() {
+  const particlesInit = useCallback(async (engine: any) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <div className="fixed inset-0 z-0 pointer-events-none">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fpsLimit: 30,
+          fullScreen: false,
+          background: {
+            color: "#141414",
+          },
+          particles: {
+            number: {
+              value: 6,
+              density: { enable: true, area: 800 },
+            },
+            color: {
+              value: ["#366585", "#2A3F4D", "#112F38", "#000000", "#4D3685"],
+            },
+            shape: {
+              type: "triangle",
+            },
+            opacity: { value: 1 },
+            size: {
+              value: { min: 60, max: 240 },
+              random: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: { default: "out" },
+              speed: 1,
+            },
+            rotate: {
+              value: { min: 0, max: 5 },
+              random: true,
+            },
+          },
+        }}
+      />
+    </div>
+  );
+}
