61ea24fbacc30710ff90a7ba8dbc517bc5108837
sentimental-j <83724982+sentimental-j@users.noreply.github.com>
2025-05-03 16:53:52 -0500

    Update page.tsx

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,1 +1,7 @@
-
+export default function ContactPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center text-white">
+      <h1 className="text-4xl font-bold">Contact Page Coming Soon...</h1>
+    </div>
+  );
+}
